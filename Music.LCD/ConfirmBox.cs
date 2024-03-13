a05aeb9a56e4d6c9cb3d8e20eb745d275fd313f0
FikuSystems <GabrielKo2008@outlook.com>
2024-03-13 11:31:43 +0000

    Notification boxes renamed to consistent name, added a generic notification box to the application

---
+++ Music.LCD/ConfirmBox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music.LCD
+{
+    public partial class ConfirmBox : Form
+    {
+        public ConfirmBox()
+        {
+            InitializeComponent();
+        }
+
+        private void warningbox_Load(object sender, EventArgs e)
+        {
+            gradients();
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows background.wav");
+            player.Play();
+            int margin = 47;
+            Screen screen = Screen.FromControl(this);
+            int newX = screen.WorkingArea.Right - this.Width;
+            int newY = screen.WorkingArea.Bottom - this.Height - margin;
+            this.Location = new Point(newX, newY);
+        }
+        private void gradients()
+        {
+            panel1.Paint += (sender, e) =>
+            {
+                using (LinearGradientBrush brush = new LinearGradientBrush(
+                    panel1.ClientRectangle,
+                    Color.FromArgb(65, 167, 31),
+                    Color.FromArgb(35, 90, 16),
+                    LinearGradientMode.Vertical))
+                {
+                    e.Graphics.FillRectangle(brush, panel1.ClientRectangle); ;
+                }
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Music.LCD/ErrorBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music.LCD
+{
+    public partial class ErrorBox : Form
+    {
+
+        public ErrorBox()
+        {
+            InitializeComponent();
+
+        }
+
+        private void notisend_Load(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Windows\Media\windows foreground.wav");
+            player.Play();
+            jajo();
+            int margin = 47;
+            Screen screen = Screen.FromControl(this);
+            int newX = screen.WorkingArea.Right - this.Width;
+            int newY = screen.WorkingArea.Bottom - this.Height - margin;
+            this.Location = new Point(newX, newY);
+        }
+        private void jajo()
+        {
+            panel1.Paint += (sender, e) =>
+            {
+                using (LinearGradientBrush brush = new LinearGradientBrush(
+                    panel1.ClientRectangle,
+                    Color.FromArgb(254, 7, 7),
+                    Color.FromArgb(177, 4, 4),
+                    LinearGradientMode.Vertical))
+                {
+                    e.Graphics.FillRectangle(brush, panel1.ClientRectangle); ;
+                }
+            };
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void kext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
