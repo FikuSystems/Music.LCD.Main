c567b1e7698260aa2f798444da88b4e011e57690
NotSnorlax <bastochmal05@gmail.com>
2024-03-27 13:48:54 +0100

    small changes to installer

--- Music.LCD.Installer/Installer.cs
+++ Music.LCD.Installer/Installer.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Resources;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
 using System.IO.Compression;
-using System.Threading;
-using System.Runtime.InteropServices;
 
 namespace Music.LCD.Installer
 {
