2af9e22ccabf3671465f3de2c7a25e019745ac7d
NotSnorlax <bastochmal05@gmail.com>
2024-04-16 22:38:02 +0200

    uninstaller work in progress

--- Music.LCD.Uninstaller/Program.cs
+++ Music.LCD.Uninstaller/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,30 @@ namespace Music.LCD.Uninstaller
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+			/*
+			if (!IsRunAsAdmin())
+			{
+				ElevateProcess();
+				return;
+			}
+			*/
+			Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Uninstaller());
         }
-    }
+		static bool IsRunAsAdmin()
+		{
+			WindowsIdentity identity = WindowsIdentity.GetCurrent();
+			WindowsPrincipal principal = new WindowsPrincipal(identity);
+			return principal.IsInRole(WindowsBuiltInRole.Administrator);
+		}
+		static void ElevateProcess()
+		{
+			var exeName = Process.GetCurrentProcess().MainModule.FileName;
+			ProcessStartInfo startInfo = new ProcessStartInfo(exeName);
+			startInfo.Verb = "runas";
+			Process.Start(startInfo);
+			Application.Exit();
+		}
+	}
 }
--- Music.LCD.Uninstaller/Uninstaller.cs
+++ Music.LCD.Uninstaller/Uninstaller.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Music.LCD.Uninstaller
 {
@@ -15,6 +18,25 @@ namespace Music.LCD.Uninstaller
         public Uninstaller()
         {
             InitializeComponent();
-        }
-    }
+            BeginUninstall();
+		}
+		private void BeginUninstall()
+		{
+			//take data from registry and delete it
+			string installLocation;
+
+            installLocation = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MusicLCD", "InstallLocation", null).ToString();
+            Registry.LocalMachine.DeleteSubKeyTree(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MusicLCD");
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"Music.LCD.lnk"))
+            {
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"Music.LCD.lnk");
+			}
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu) + @"\Programs\Music.LCD.lnk"))
+            {
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu) + @"\Programs\Music.LCD.lnk");
+            }
+            this.Close();
+		}
+	} 
+    
 }
