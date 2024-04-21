5245ebb42fc8f7b8cbd0d872ce74dbda973d33e2
NotSnorlax <bastochmal05@gmail.com>
2024-04-21 23:31:29 +0200

    small change

--- Music.LCD.Installer/Installer.cs
+++ Music.LCD.Installer/Installer.cs
@@ -381,6 +381,7 @@ namespace Music.LCD.Installer
                 key.SetValue("HelpLink", @"https:\\www.fikusystems.com\");
                 key.SetValue("InstallLocation", choosenPath);
                 key.SetValue("URLInfoAbout", @"https:\\www.fikusystems.com\");
+                key.SetValue("DisplayVersion", "0.1.0.1");
                 key.Close();
 				if (silentStart)
                 {
@@ -460,7 +461,7 @@ namespace Music.LCD.Installer
                 key.SetValue("Publisher", "FikuSystems");
                 key.SetValue("HelpLink", @"https:\\www.fikusystems.com\");
                 key.SetValue("InstallLocation", choosenPath);
-                key.SetValue("DisplayVersion", "0.1.0.0");
+                key.SetValue("DisplayVersion", "0.1.0.1");
                 key.SetValue("URLInfoAbout", @"https:\\www.fikusystems.com\");
                 key.Close();
             }
